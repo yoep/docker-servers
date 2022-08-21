c1cf9ea580a1468af59e50b989740bbcaac7a9c2
jorgi <jorgi.leus@hotmail.com>
2022-08-21 22:19:54 +0200

    Add OceanBlock module

---
+++ oceanblock/start-server.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Verify if the required env vars are present
+if [[ -z "${JAVA_MEMORY_LIMIT}" ]]; then
+  echo "Missing environment variable JAVA_MEMORY_LIMIT"
+  exit 1;
+fi
+if [[ -z "${JAVA_MEMORY_REQUEST}" ]]; then
+  echo "Missing environment variable JAVA_MEMORY_REQUEST"
+  exit 1;
+fi
+if [[ ! -f "server.properties" ]]; then
+  echo "Missing server.properties configuration"
+  exit 1;
+fi
+
+echo "Auto agreement to the Mojang EULA available at https://account.mojang.com/documents/minecraft_eula"
+echo "eula=true" > eula.txt
+java -javaagent:log4jfix/Log4jPatcher-1.0.0.jar -XX:+UnlockExperimentalVMOptions -Xmx${JAVA_MEMORY_LIMIT}M -Xms${JAVA_MEMORY_REQUEST}M @user_jvm_args.txt @libraries/net/minecraftforge/forge/1.18.2-40.1.67/unix_args.txt nogui
