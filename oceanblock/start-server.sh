ce88d9b3cb78b0eeb1d95e4c7a8fea81de52bbec
jorgi <jorgi.leus@hotmail.com>
2022-08-22 20:56:02 +0200

    fix start-server script

--- oceanblock/start-server.sh
+++ oceanblock/start-server.sh
@@ -1,19 +1,11 @@
+
 #!/bin/bash
-
-# Verify if the required env vars are present
-if [[ -z "${JAVA_MEMORY_LIMIT}" ]]; then
-  echo "Missing environment variable JAVA_MEMORY_LIMIT"
-  exit 1;
+if ! grep -q "eula=true" eula.txt; then
+    echo "Do you agree to the Mojang EULA available at https://account.mojang.com/documents/minecraft_eula ?"
+    read -N 1 -p "[y/n] " EULA
+    if [ "$EULA" = "y" ]; then
+        echo "eula=true" > eula.txt
+        echo
+    fi
 fi
-if [[ -z "${JAVA_MEMORY_REQUEST}" ]]; then
-  echo "Missing environment variable JAVA_MEMORY_REQUEST"
-  exit 1;
-fi
-if [[ ! -f "server.properties" ]]; then
-  echo "Missing server.properties configuration"
-  exit 1;
-fi
-
-echo "Auto agreement to the Mojang EULA available at https://account.mojang.com/documents/minecraft_eula"
-echo "eula=true" > eula.txt
-java -javaagent:log4jfix/Log4jPatcher-1.0.0.jar -XX:+UnlockExperimentalVMOptions -Xmx${JAVA_MEMORY_LIMIT}M -Xms${JAVA_MEMORY_REQUEST}M @user_jvm_args.txt @libraries/net/minecraftforge/forge/1.18.2-40.1.67/unix_args.txt nogui
+"java" -javaagent:log4jfix/Log4jPatcher-1.0.0.jar -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -Xmx6144M -Xms4096M -jar forge-1.16.5-36.2.39.jar nogui
