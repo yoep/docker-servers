03d48f7276d7ffebc2ea7762f03599b3df6d0f9c
yoep <yoeri.peeters10@telenet.be>
2023-09-28 13:08:54 +0200

    Cleanup of spaceengineers setup

--- spaceengineers/start_server.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-log_location=${INSTALL_LOCATION}/config/logs/server-$(date +%s).log
-cd ${WINE_LOCATION}/drive_c/users/${whoami}/Desktop/spaceengineers/DedicatedServer64
-echo "Starting SpaceEngineersDedicated..."
-echo "Check log ${log_location} for more info"
-# Show the log in the console + log it immediately to a file
-WINEPREFIX=${WINE_LOCATION} wine SpaceEngineersDedicated.exe -noconsole -ignorelastsession -checkAlive | tee ${log_location}
-echo "Server has been stopped"
