5d17aa2ac3df532cafbb829458e2af5c434a0340
yoep <yoeri.peeters10@telenet.be>
2023-09-28 12:36:51 +0200

    Used prebuilt image

--- spaceengineers/start_server.sh
+++ spaceengineers/start_server.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
-# **************************
-# EDIT CONFIG SETTINGS BELOW
-# **************************
-# Installation config
-install_location=${INSTALL_LOCATION}
-world_name=${WORLD_NAME}
 
-log_location=${install_location}/config/logs/server-$(date +%s).log
+log_location=${INSTALL_LOCATION}/config/logs/server-$(date +%s).log
 cd ${WINE_LOCATION}/drive_c/users/${whoami}/Desktop/spaceengineers/DedicatedServer64
 echo "Starting SpaceEngineersDedicated..."
 echo "Check log ${log_location} for more info"
