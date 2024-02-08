c071ab24f9125b6d9c2b9d25cfc7dc88417a2dc6
yoep <yoeri.peeters10@telenet.be>
2024-02-08 20:27:34 +0100

    Moved env vars to Dockerfile

--- wreckfest/start.sh
+++ wreckfest/start.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-export WINEARCH=win32
-export WINEPREFIX=${HOME}/.wreckfest
-export CONFIG_FILE=server_config.cfg
-export CONFIG_FILE_LOCATION="config/${CONFIG_FILE}"
-
 if [[ ! -e ${CONFIG_FILE_LOCATION} ]]; then
   echo "Copying config file ${CONFIG_FILE} to ${CONFIG_FILE_LOCATION}";
   cp -v ${CONFIG_FILE} ${CONFIG_FILE_LOCATION};
